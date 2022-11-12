8a7da46175ce1653f61453e719367886181e5ff8
Nirmal Ariyathilake <nirmal.ariyathilake@gmail.com>
2022-11-13 05:20:13 +0530

    Initial commit

---
+++ lib/country_ip.dart
@@ -0,0 +1,7 @@
+library country_ip;
+
+class CountryIp {
+  static Future<String> findCountry() async {
+    return '';
+  }
+}
---
+++ test/country_ip_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:country_ip/country_ip.dart';
+
+void main() {
+  
+}
